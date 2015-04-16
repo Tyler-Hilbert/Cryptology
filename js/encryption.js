db2a8deaf1ec530fb0649aaa4cee9b79a00d5675
Tyler-Hilbert <tylerjoshuahilbert@gmail.com>
2015-04-16 13:43:38 -0400

    Added one time pad encryption

--- js/encryption.js
+++ js/encryption.js
@@ -3,6 +3,7 @@ function encrypt() {
 	
 	caesar(message);
 	polyalphabetic(message);
+	oneTimePad(message);
 }
 
 function caesar(message) {
@@ -35,4 +36,34 @@ function polyalphabetic(message) {
 	}
 	
 	document.getElementById('polyalphabetic-encrypt').textContent = "Polyalphabetic cipher: Key: " + seed + ": " + encryptedMessage;	
+}
+
+function oneTimePad(message) {
+	// create list of
+	var key = [];
+	for (var i = 0; i < message.length; i++) {
+		key.push(Math.floor(Math.random() * 94));
+	}
+	
+	var encryptedMessage = "";
+	
+	// Encrypt
+	for (var i = 0; i < message.length; i++) {
+		// Shift the ASCII value within the range of 32 to 126(the valid ASCII values).
+		var charAscii = ((message.charCodeAt(i) + key[i] - 32) % 95) + 32;
+		encryptedMessage += String.fromCharCode(charAscii);
+	}
+	
+	// Output
+	document.getElementById('one-time-pad-key').innerHTML = "Key: ";
+	for (var i = 0; i < key.length; i++) {
+		var output = key[i];
+		if (i < key.length - 1) {
+			output += ", ";
+		}
+		document.getElementById('one-time-pad-key').innerHTML += output;
+	}
+	
+	document.getElementById('one-time-pad-encrypt').textContent = "One time pad cipher: " + encryptedMessage;	
+	
 }
