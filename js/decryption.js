130e3485e6fceaabcd61b329219481bf849bce28
Tyler Hilbert <tylerjoshuahilbert@gmail.com>
2016-10-12 00:06:46 -0400

    Moved remaining native JS to jquery

--- js/decryption.js
+++ js/decryption.js
@@ -1,10 +1,9 @@
 function decrypt() {
-	var cipherSelect = document.getElementById("cipher");
-	var cipher = cipherSelect.options[cipherSelect.selectedIndex].value;
-	
-	var message = document.getElementById('encrypted-message').value;
-	var key = document.getElementById('key').value;
-	
+	var cipher = $('#cipher').find(":selected").val();
+
+	var message = $('#encrypted-message').val();
+	var key = $('#key').val();
+
 	if (cipher === "caesar") {
 		caesar(message, key);
 	} else if(cipher === "polyalphabetic") {
@@ -17,43 +16,43 @@ function decrypt() {
 
 function caesar(message, key) {
 	var decryptedMessage = "";
-	
+
 	for (var i = 0; i < message.length; i++) {
 		if (message.charCodeAt(i) == 32) { // No shift occurred
 			decryptedMessage += " ";
-		} else { 
+		} else {
 			var low = 33; // The lowest ASCII value possible
 			var high = 126; // The highest ASCII value possible
 			decryptedMessage += shift(message[i], key, 33, 126);
 		}
 	}
-	
-	document.getElementById('decrypted-message').textContent = "Decrypted message: " + decryptedMessage;
+
+	$('#decrypted-message').html("Decrypted message: " + decryptedMessage);
 }
 
 
 function polyalphabetic(message, seed) {
 	var decryptedMessage = "";
-	
+
 	for (var i = 0; i < message.length; i++) {
 		var low = 32; // Lowest possible ASCII value
 		var high = 126; // Highest possible ASCII value
 		decryptedMessage += shift(message[i], seed.charCodeAt(i%seed.length) - 32, low, high);
 	}
-	
-	document.getElementById('decrypted-message').textContent = "Decrypted message: " + decryptedMessage;
+
+	$('#decrypted-message').html("Decrypted message: " + decryptedMessage);
 }
 
 function otp(message, seed) {
 	var keys = seed.split(",");
 	var decryptedMessage = "";
-	
+
 	for (var i = 0; i < keys.length && i < message.length; i++) {
 		var key = parseInt(keys[i].trim());
 		decryptedMessage += shift(message[i], key, 32, 126);
 	}
-	
-	document.getElementById('decrypted-message').textContent = "Decrypted message: " + decryptedMessage;
+
+	$('#decrypted-message').html("Decrypted message: " + decryptedMessage);
 }
 
 /**
@@ -61,14 +60,14 @@ function otp(message, seed) {
 */
 function shift(chr, key, low, high) {
 	var shiftedValue = chr.charCodeAt(0) - key;
-	
+
 	while (shiftedValue < low) {
 		shiftedValue += high - low + 1;
 	}
-	
+
 	while (shiftedValue > high) {
 		shiftedValue -= high - low + 1;
 	}
-	
+
 	return String.fromCharCode(shiftedValue);
 }
--- js/encryption.js
+++ js/encryption.js
@@ -1,7 +1,7 @@
 function encrypt() {
-	var message = document.getElementById('message').value;
-	var key = document.getElementById('key').value;
-	
+	var message = $('#message').val();
+	var key = $('#key').val();
+
 	caesar(message, key);
 	polyalphabetic(message, key);
 	oneTimePad(message, key);
@@ -14,27 +14,27 @@ function caesar(message, key) {
 	for (var i = 0; i < message.length; i++) {
 		if (message.charCodeAt(i) == 32) { // Don't shift spaces to give away the shift key
 			encryptedMessage += "  ";
-		} else { 
+		} else {
 			// Shift the ASCII value within the range of 33 to 126(the valid ASCII values).
 			var charAscii = ((message.charCodeAt(i) - 33 + key) % 94) + 33;
 			encryptedMessage += String.fromCharCode(charAscii);
 		}
 	}
-	
-	document.getElementById('caesar-encrypt').textContent = encryptedMessage;
+
+	$('#caesar-encrypt').html(encryptedMessage);
 }
 
 function polyalphabetic(message, seed) {
 	var encryptedMessage = "";
 
-	
+
 	for (var i = 0; i < message.length; i++) {
 		// Shift the ASCII value within the range of 32 to 126(the valid ASCII values).
 		var charAscii = ((message.charCodeAt(i) + seed.charCodeAt(i % seed.length) -  64) % 95) + 32;
 		encryptedMessage += String.fromCharCode(charAscii);
 	}
-	
-	document.getElementById('polyalphabetic-encrypt').textContent = encryptedMessage;	
+
+	$('#polyalphabetic-encrypt').html(encryptedMessage);
 }
 
 function oneTimePad(message, key) {
@@ -43,16 +43,16 @@ function oneTimePad(message, key) {
 	for (var i = 0; i < keys.length; i++) {
 		keys[i] = parseInt(keys[i].trim());
 	}
-	
+
 	var encryptedMessage = "";
-	
+
 	// Encrypt
 	for (var i = 0; i < message.length && i < keys.length; i++) {
 		// Shift the ASCII value within the range of 32 to 126(the valid ASCII values).
 		var charAscii = ((message.charCodeAt(i) + keys[i] - 32) % 95) + 32;
 		encryptedMessage += String.fromCharCode(charAscii);
 	}
-	
-	document.getElementById('one-time-pad-encrypt').textContent = encryptedMessage;	
-	
+
+	$('#one-time-pad-encrypt').html(encryptedMessage);
+
 }
